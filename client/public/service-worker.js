eeb2219ec35419bc32d02b592bb013bbcb2af9e6
muralee <kananhere@gmail.com>
2019-10-03 22:49:20 +0800

    Update service-worker.js

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "weather-cache-v1";
-
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = ["/offline.html"];
 
 window.self.addEventListener("install", evt => {
@@ -21,7 +21,7 @@ window.self.addEventListener("activate", evt => {
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
-          if (key !== CACHE_NAME) {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("[ServiceWorker] Removing old cache", key);
             return caches.delete(key);
           }
@@ -39,10 +39,32 @@ window.self.addEventListener("fetch", evt => {
     // Not a page navigation, bail.
     return;
   }
+  if (evt.request.url.includes("/forecast/")) {
+    console.log("[Service Worker] Fetch (data)", evt.request.url);
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      })
+    );
+    return;
+  }
+
   evt.respondWith(
-    fetch(evt.request).catch(async () => {
-      const cache = await caches.open(CACHE_NAME);
-      return cache.match("offline.html");
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
     })
   );
 });
